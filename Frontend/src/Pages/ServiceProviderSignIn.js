b7ffca1145058a79f4eed21a48b3bff6f01ebec5
Akeem Bello <belloakeem07@gmail.com>
2023-11-11 21:49:19 -0700

    Made changes to the service provider sign in page

--- Frontend/src/Pages/ServiceProviderSignIn.js
+++ Frontend/src/Pages/ServiceProviderSignIn.js
@@ -26,8 +26,8 @@ const ServiceProviderSignIn = () => {
                <div className="col-7 mx-auto mt-5 shadow p-5">
                    <h3>Sign-In</h3>
                    <div className='text-center'>{message}</div>
-                      <input className='form form-control my-2' type="text" placeholder='Enter your email' value={email} onChange={(e)=>setemail(e.target.value)}/>
-                      <input className='form form-control my-2' type="password" placeholder='Enter your password' value={password} onChange={(e)=>setpassword(e.target.value)}/>
+                      <input className='form form-control my-2' type="text" placeholder='E-mail' value={email} onChange={(e)=>setemail(e.target.value)}/>
+                      <input className='form form-control my-2' type="password" placeholder='Password' value={password} onChange={(e)=>setpassword(e.target.value)}/>
                       <button type='submit' className='btn btn-primary my-2 w-100' onClick={signIn}>Sign In</button> 
                    <div className='mt-2'><p>By continuing, you agree to HairConnect's <u>Conditions of Use</u> and <u>Privacy Notice.</u></p></div>
                    <div>
