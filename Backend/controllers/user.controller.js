c0f2ff0ca33c2b90cfad33d3fadb625ff1d80c54
Akeem Bello <belloakeem07@gmail.com>
2023-11-13 20:10:21 -0700

    Fixed the mongoose error

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,26 +1,23 @@
 const userModel = require('../models/user.model');
 
-const registerUser = (req, res)=>{
+const registerUser = async (req, res)=>{
     const userDetails = req.body;
     const email = userDetails.email;
-    userModel.findOne({email:email}, (err, result)=>{
-        if(err){
-            res.status(500).send({message: 'Internal Server Error', status:false});
-        }else{
-            if(result){
-                res.send({message: 'E-mail already exists.', status:false})
-            }else{
-                let form = new userModel(userDetails);
-                form.save((err)=>{
-                    if(err){
-                        res.status(500).send({message: 'Sign up failed. Please, try again.', status:false})
-                    }else{
-                        res.send({message: 'Sign up successful', status:true})
-                    }
-                })
-            }
+    try {
+        const result = await userModel.findOne({ email: email }).exec();
+      
+        if (result) {
+          res.send({ message: 'E-mail already exists.', status: false });
+        } else {
+          const form = new userModel(userDetails);
+          await form.save();
+          res.send({ message: 'Sign up successful', status: true });
         }
-    })
+      } catch (err) {
+        console.error(err);
+        res.status(500).send({ message: 'Internal Server Error', status: false });
+      }
+      
 }
 
 module.exports = {
