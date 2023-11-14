558616e08a6e9b97ff75cd0a48e16cf3982d8284
Akeem Bello <belloakeem07@gmail.com>
2023-11-13 19:53:52 -0700

    Created the router for user sign up

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,4 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
+const userController = require('../controllers/user.controller');
+router.post('/signup', userController.registerUser);
 module.exports = router;
