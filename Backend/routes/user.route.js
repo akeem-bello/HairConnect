34afdadd78f0a5ee27b567ff1a07eb2dec45d9c5
Akeem Bello <belloakeem07@gmail.com>
2023-11-14 19:18:24 -0700

    Created the backend route for user sign in

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -2,4 +2,5 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
 router.post('/signup', userController.registerUser);
+router.post('/signin', userController.userSignIn);
 module.exports = router;
