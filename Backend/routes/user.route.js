0a7b68ae27f704508087e92860478c3789572624
Akeem Bello <belloakeem07@gmail.com>
2023-11-14 20:13:49 -0700

    Created the route for getting user dashboard at the backend

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const userController = require('../controllers/user.controller');
 router.post('/signup', userController.registerUser);
 router.post('/signin', userController.userSignIn);
+router.get('/dashboard', userController.getUserDashboard);
 module.exports = router;
