7df7a3b1f235a435f1963bab5627e65e9fb872bc
Akeem Bello <belloakeem07@gmail.com>
2023-11-14 19:15:17 -0700

    Created the user sign in function at the backend

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -20,6 +20,36 @@ const registerUser = async (req, res)=>{
       
 }
 
+const userSignIn = (req, res)=>{
+  const userDetails = req.body;
+  const email = userDetails.email;
+  const password = userDetails.password;
+  userModel.findOne({email:email}, (err, user)=>{
+    if(err){
+      console.log(err);
+      res.status(500).send({message: 'Internal Server Error', status:false});
+    }else{
+      if(!user){
+        res.send({message: 'E-mail does not exist, kindly create an account', status:false})
+      }else{
+        user.validatePassword(password, (err, same)=>{
+          if(err){
+              res.status(500).send({message: 'Internal Server Error.', status: false});
+          }else{
+              if(!same){
+                  res.send({message: 'Some of your information is not correct. Please try again.', status: false});
+              }else{
+                  const hairConnectToken = jwt.sign({email}, SECRET, {expiresIn: '10h'})
+                  res.send({message: 'Sign in successful.', status: true, hairConnectToken});
+              }
+          }
+      })
+      }
+    }
+  })
+}
+
 module.exports = {
-    registerUser
+    registerUser,
+    userSignIn
 }
