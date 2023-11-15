8a49365050f36ce1e67ce3bd1881edd3ebf0295e
Akeem Bello <belloakeem07@gmail.com>
2023-11-14 20:09:00 -0700

    Created function for getting user dashboard at the backend

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -5,17 +5,16 @@ const jwt = require('jsonwebtoken');
 const registerUser = async (req, res)=>{
     const userDetails = req.body;
     const email = userDetails.email;
-    try {
+    try{
         const result = await userModel.findOne({ email: email }).exec();
-      
-        if (result) {
+        if(result){
           res.send({ message: 'E-mail already exists.', status: false });
-        } else {
+        }else{
           const form = new userModel(userDetails);
           await form.save();
           res.send({ message: 'Sign up successful', status: true });
         }
-      } catch (err) {
+      }catch(err){
         console.error(err);
         res.status(500).send({ message: 'Internal Server Error', status: false });
       }
@@ -26,32 +25,42 @@ const userSignIn = async (req, res)=>{
   const userDetails = req.body;
   const email = userDetails.email;
   const password = userDetails.password;
-  
-  try {
+  try{
     const user = await userModel.findOne({ email: email });
-  
-    if(!user) {
+    if(!user){
       res.send({ message: 'E-mail does not exist, kindly create an account', status: false });
       return;
     }
-  
     const isPasswordValid = await user.validatePassword(password);
-  
-    if(!isPasswordValid) {
+    if(!isPasswordValid){
       res.send({ message: 'Your password is incorrect, please try again.', status: false });
       return;
     }
-  
     const hairConnectToken = jwt.sign({ email }, SECRET, { expiresIn: '10h' });
     res.send({ message: 'Sign in successful.', status: true, hairConnectToken });
-  } catch (err) {
+  }catch(err){
     console.error(err);
     res.status(500).send({ message: 'Internal Server Error', status: false });
   }
   
 }
 
+const getUserDashboard = async (req, res) => {
+  try{
+    const hairConnectToken = req.headers.authorization.split(' ')[1];
+    const verifiedToken = await jwt.verify(hairConnectToken, SECRET);
+    const email = verifiedToken.email;
+    const userDetails = await userModel.findOne({ email: email });
+    res.send({status: true, userDetails });
+  }catch(err) {
+    console.error(err);
+    res.send({ message: 'jwt failed', err, status: false });
+  }
+};
+
+
 module.exports = {
     registerUser,
-    userSignIn
+    userSignIn,
+    getUserDashboard
 }
